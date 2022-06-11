c055f4cb88531de14ed5bdf2b3458c163053642e
Thotsuya <55269430+Thotsuya@users.noreply.github.com>
2022-06-11 09:21:23 -0600

    Route redirect to login

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use Inertia\Inertia;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::redirect('/','/login');
+
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
